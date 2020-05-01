3257afee690a5a08f6f495add1ca6a71ae335634
grace22411 <olayinkagrace112.ob@gmail.com>
2020-04-30 18:15:15 -0700

    updates made

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
   });
 
 
-  $(".filter-button").click(function() {
+  $(".filter-button").click(function () {
     var value = $(this).attr('data-filter');
 
     if (value == "all") {
@@ -64,7 +64,7 @@ $(document).ready(function () {
   $(this).addClass("active");
 
 
-  $('.dropdown-menu button').click(function() {
+  $('.dropdown-menu button').click(function () {
     console.log($(this).attr('data-value'));
     $(this).closest('.dropdown').find('input.countrycode')
       .val($(this).attr('data-value'));
@@ -80,9 +80,51 @@ $(document).ready(function () {
       input.attr("type", "password");
     }
   });
+  toastr.options = {
+    'closeButton': true,
+    'debug': false,
+    'newestOnTop': false,
+    'progressBar': false,
+    'positionClass': 'toast-top-center',
+    'preventDuplicates': false,
+    'showDuration': '0',
+    'hideDuration': '0',
+    'timeOut': '5000',
+    'extendedTimeOut': '1000',
+    'showEasing': 'swing',
+    'hideEasing': 'linear',
+    'showMethod': 'fadeIn',
+    'hideMethod': 'fadeOut',
+  }
 
+  $("#copy-tex").click(function(tex){
+      var copyText = $("#myInput");
+      copyText.select();
+      document.execCommand("copy");
+      toastr.success('Link Copied!');
+   });
 
+   $("#copy-texted").click(function(tex){
+    var copyText = $("#myInput");
+    copyText.select();
+    document.execCommand("copy");
+    toastr.success('Link Copied!');
+ });
 
+   $("#camp-copy").click(function(tex){
+    var copyText = $("#camp-text");
+    copyText.select();
+    document.execCommand("copy");
+    toastr.success('Text Copied!');
+ });
+ 
+
+//    $("#camp-copy").click(function(){
+//     var copyText = $(".camp-text");
+//     copyText.select();
+//     document.execCommand("copy");
+//     alert("Copied the text: ");
+//  });
   var selectedPreference = [];
   //const category = element.data("name")
 
@@ -114,74 +156,38 @@ $(document).ready(function () {
     });
   });
 
-
-
-  $('.click-button').click(function (){
-
-    var $input = $(this).closest('div').find('.copy-text');
-
-    /* Select the text field */
-    $input.select();
-
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-
-    // $(this)
-    //     .popup({
-    //         title    : 'Successfully copied to clipboard!',
-    //         content  : 'Share this link with your receiver or distributor so they can confirm their order.',
-    //         on: 'manual',
-    //         exclusive: true
-    //     })
-    //     .popup('show')
-    // ;
-
-    // // Hide popup after 5 seconds
-    // delayPopup(this);
-
-
-});
-  // var element = $('#p1')
-  // $('.fa-copy').click(function (element) {
-  //   var $temp = $("<input>");
-  //   $("body").append($temp);
-  //   $temp.val($(element).text()).select();
-  //   document.execCommand("copy");
-  //   $temp.remove();
-  // });
-
-
   var banks = [
-    { "id": "1", "name": "Access Bank" ,"code":"044" },
-    { "id": "2", "name": "Citibank","code":"023" },
-    { "id": "3", "name": "Diamond Bank","code":"063" },
-    { "id": "4", "name": "Dynamic Standard Bank","code":"" },
-    { "id": "5", "name": "Ecobank Nigeria","code":"050" },
-    { "id": "6", "name": "Fidelity Bank Nigeria","code":"070" },
-    { "id": "7", "name": "First Bank of Nigeria","code":"011" },
-    { "id": "8", "name": "First City Monument Bank","code":"214" },
-    { "id": "9", "name": "Guaranty Trust Bank","code":"058" },
-    { "id": "10", "name": "Heritage Bank Plc","code":"030" },
-    { "id": "11", "name": "Jaiz Bank","code":"301" },
-    { "id": "12", "name": "Keystone Bank Limited","code":"082" },
-    { "id": "13", "name": "Providus Bank Plc","code":"101" },
-    { "id": "14", "name": "Polaris Bank","code":"076" },
-    { "id": "15", "name": "Stanbic IBTC Bank Nigeria Limited","code":"221" },
-    { "id": "16", "name": "Standard Chartered Bank","code":"068" },
-    { "id": "17", "name": "Sterling Bank","code":"232" },
-    { "id": "18", "name": "Suntrust Bank Nigeria Limited","code":"100" },
-    { "id": "19", "name": "Union Bank of Nigeria","code":"032" },
-    { "id": "20", "name": "United Bank for Africa","code":"033" },
-    { "id": "21", "name": "Unity Bank Plc","code":"215" },
-    { "id": "22", "name": "Wema Bank","code":"035" },
-    { "id": "23", "name": "Zenith Bank","code":"057" }
-];
+    { "id": "1", "name": "Access Bank", "code": "044" },
+    { "id": "2", "name": "Citibank", "code": "023" },
+    { "id": "3", "name": "Diamond Bank", "code": "063" },
+    { "id": "4", "name": "Dynamic Standard Bank", "code": "" },
+    { "id": "5", "name": "Ecobank Nigeria", "code": "050" },
+    { "id": "6", "name": "Fidelity Bank Nigeria", "code": "070" },
+    { "id": "7", "name": "First Bank of Nigeria", "code": "011" },
+    { "id": "8", "name": "First City Monument Bank", "code": "214" },
+    { "id": "9", "name": "Guaranty Trust Bank", "code": "058" },
+    { "id": "10", "name": "Heritage Bank Plc", "code": "030" },
+    { "id": "11", "name": "Jaiz Bank", "code": "301" },
+    { "id": "12", "name": "Keystone Bank Limited", "code": "082" },
+    { "id": "13", "name": "Providus Bank Plc", "code": "101" },
+    { "id": "14", "name": "Polaris Bank", "code": "076" },
+    { "id": "15", "name": "Stanbic IBTC Bank Nigeria Limited", "code": "221" },
+    { "id": "16", "name": "Standard Chartered Bank", "code": "068" },
+    { "id": "17", "name": "Sterling Bank", "code": "232" },
+    { "id": "18", "name": "Suntrust Bank Nigeria Limited", "code": "100" },
+    { "id": "19", "name": "Union Bank of Nigeria", "code": "032" },
+    { "id": "20", "name": "United Bank for Africa", "code": "033" },
+    { "id": "21", "name": "Unity Bank Plc", "code": "215" },
+    { "id": "22", "name": "Wema Bank", "code": "035" },
+    { "id": "23", "name": "Zenith Bank", "code": "057" }
+  ];
 
-$.each(banks, function(key, val) {
-  $("#bank_selected").append("<option value='" + key + "'>" + val.name + "</option>");
-});
+  $.each(banks, function (key, val) {
+    $("#bank_selected").append("<option value='" + key + "'>" + val.name + "</option>");
+  });
 
-
+    
+  
 
 });
 
